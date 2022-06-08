ce19488da93610d59530dc584f2401a0c5547e0a
Maxivich <maxivlevich@gmail.com>
2022-06-08 21:12:42 +0300

    hw4

--- src/main/java/com/example/app/Controller.java
+++ src/main/java/com/example/app/Controller.java
@@ -6,6 +6,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class Controller {
     @FXML
     private TextArea textField;
@@ -13,6 +20,17 @@ public class Controller {
     private TextField userText;
 
     public void clickCheckButton(ActionEvent actionEvent) {
+        Date date = new Date();
+        String time ;
+        String strDateFormat = "hh:mm:ss a";
+        DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
+        String formattedDate= dateFormat.format(date);
+
+        final String text = userText.getText();
+        textField.appendText(formattedDate+"username : "+text+"\n");
+        userText.clear();
+        userText.requestFocus();
+
     }
 
 }
